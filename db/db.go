17f9c464d4f555054a95e4d2a181756791d52f38
kautsar_masuara <kautsarmasuara@gmail.com>
2022-09-06 20:53:41 +0800

    success connect to mongodb

---
+++ db/db.go
@@ -0,0 +1,36 @@
+package mongoDB
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var ctx = context.Background()
+
+type student struct {
+	Name  string `bson:"name"`
+	Grade int    `bson:"Grade"`
+}
+
+func ConnectDB() *mongo.Collection {
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("testing").Collection("data")
+
+	return collection
+}
--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	mongoDB "Golang-upload-file-mongodb/db"
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type test_struct struct {
-	Test string
+var collection = mongoDB.ConnectDB()
+
+type Data struct {
+	ID   primitive.ObjectID
+	Name string
 }
 
 func image(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
 	if r.URL.Path != "/" {
 		http.Error(w, "404 page not found", http.StatusNotFound)
 	}
 	switch r.Method {
 	case "GET":
+		cur, err := collection.Find(ctx, bson.M{})
+		if err != nil {
+			panic(err)
+		}
+		defer cur.Close(ctx)
+
+		for cur.Next(ctx) {
+			var row Data
+			err := cur.Decode(&row)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(row)
+		}
+
 		fmt.Fprintf(w, "Hello world")
 	case "POST":
 		fmt.Fprintf(w, "Ini adalah %v", r.FormValue("message"))
@@ -32,6 +56,7 @@ func image(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	mongoDB.ConnectDB()
 	http.HandleFunc("/", image)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
